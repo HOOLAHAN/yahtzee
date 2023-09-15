7b24999b7122307da41b2c94fe079f50a209b66c
Iain <iain.hoolahan@gmail.com>
2023-09-15 12:54:36 +0100

    unit test utility functions of gameControl

---
+++ src/tests/gameControl.test.tsx
@@ -0,0 +1,93 @@
+import { canLockInScore, lockInScore, resetGame, startNewRound } from '../functions/gameControl';
+import { ScoreEntry } from '../functions/types';
+
+describe("canLockInScore function", () => {
+  it("should return false if not rolled", () => {
+    expect(canLockInScore('ThreeOfAKind', false, new Set())).toBe(false);
+  });
+
+  it("should return false if category already used", () => {
+    expect(canLockInScore('ThreeOfAKind', true, new Set(['ThreeOfAKind']))).toBe(false);
+  });
+
+  it("should return true otherwise", () => {
+    expect(canLockInScore('ThreeOfAKind', true, new Set(['FourOfAKind']))).toBe(true);
+  });
+});
+
+describe("lockInScore function", () => {
+  const setFunctions = Array.from({ length: 11 }, () => jest.fn());
+
+  const [
+    setUsedCategories,
+    setTotalScore,
+    setScoreHistory,
+    startNewRoundFn,
+    setCurrentScore,
+    setHasRolled,
+    setDice,
+    setRollsLeft,
+    setHeldDice,
+  ] = setFunctions;
+
+  const initialDice = [1, 2, 3, 4, 5];
+  const currentScore = 0;
+  const totalScore = 100;
+  const scoreHistory: ScoreEntry[] = [];
+  const usedCategories = new Set<string>();
+
+  const dice = [2, 3, 3, 3, 6];
+
+  const calculateScoreFunction = jest.fn().mockReturnValue(9);
+
+  it("should lock in a new score", () => {
+    lockInScore(
+      'ThreeOfAKind', usedCategories, setUsedCategories,
+      dice, setTotalScore, totalScore, setScoreHistory,
+      scoreHistory, startNewRoundFn, setCurrentScore, setHasRolled,
+      setDice, setRollsLeft, setHeldDice, initialDice, currentScore, calculateScoreFunction
+    );
+
+    expect(setUsedCategories).toHaveBeenCalledWith(new Set(['ThreeOfAKind']));
+    expect(setTotalScore).toHaveBeenCalledWith(NaN);
+    expect(setScoreHistory).toHaveBeenCalled();
+    expect(startNewRoundFn).toHaveBeenCalled();
+  });
+});
+
+describe("resetGame function", () => {
+  const setFunctions = Array.from({ length: 8 }, () => jest.fn());
+  const [setDice, setRollsLeft, setHeldDice, setCurrentScore, setScoreHistory, setHasRolled, setTotalScore, setUsedCategories] = setFunctions;
+  const initialDice = [1, 2, 3, 4, 5];
+
+  it("should reset game states", () => {
+    resetGame(setDice, setRollsLeft, setHeldDice, setCurrentScore, setScoreHistory, setHasRolled, setTotalScore, initialDice, setUsedCategories);
+
+    expect(setDice).toHaveBeenCalledWith(initialDice);
+    expect(setRollsLeft).toHaveBeenCalledWith(3);
+    expect(setHeldDice).toHaveBeenCalledWith(new Set());
+    expect(setCurrentScore).toHaveBeenCalledWith(0);
+    expect(setScoreHistory).toHaveBeenCalledWith([]);
+    expect(setHasRolled).toHaveBeenCalledWith(false);
+    expect(setTotalScore).toHaveBeenCalledWith(0);
+    expect(setUsedCategories).toHaveBeenCalledWith(new Set<string>());
+  });
+});
+
+describe("startNewRound function", () => {
+  const setFunctions = Array.from({ length: 7 }, () => jest.fn());
+  const [setDice, setRollsLeft, setHeldDice, setCurrentScore, setHasRolled, setTotalScore] = setFunctions;
+  const initialDice = [1, 2, 3, 4, 5];
+  const totalScore = 100;
+  const currentScore = 9;
+
+  it("should reset round states", () => {
+    startNewRound(setDice, setRollsLeft, setHeldDice, setCurrentScore, setHasRolled, setTotalScore, initialDice, totalScore, currentScore);
+
+    expect(setDice).toHaveBeenCalledWith(initialDice);
+    expect(setRollsLeft).toHaveBeenCalledWith(3);
+    expect(setHeldDice).toHaveBeenCalledWith(new Set());
+    expect(setCurrentScore).toHaveBeenCalledWith(0);
+    expect(setHasRolled).toHaveBeenCalledWith(false);
+  });
+});
