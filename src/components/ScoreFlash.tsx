7b101d31f767125ac7d49be6f97564dc5beb0bbe
Iain <iain.hoolahan@gmail.com>
2023-09-23 13:35:22 +0200

    added packlock icon

--- src/components/ScoreFlash.tsx
+++ src/components/ScoreFlash.tsx
@@ -1,6 +1,8 @@
 // ScoreFlash.tsx
 
 import React, { useEffect, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLock } from '@fortawesome/free-solid-svg-icons';
 
 interface ScoreFlashProps {
   category: string;
@@ -27,6 +29,7 @@ const ScoreFlash: React.FC<ScoreFlashProps> = ({ category, duration = 2000, show
   return (
     <div className="fixed inset-0 flex items-center justify-center">
       <div className="bg-white text-2xl p-4 rounded shadow-lg animate-fadeInScaleUpFadeOut">
+          <FontAwesomeIcon icon={faLock} className="pr-2" />
           {category.replace(/([A-Z])/g, ' $1').trim()}
       </div>
     </div>
