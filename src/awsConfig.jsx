09a8e6b4dd8728d15d2aa05c583be753f6ae197b
Iain <iain.hoolahan@gmail.com>
2024-02-25 13:16:47 +0000

    removed comments

--- src/awsConfig.jsx
+++ src/awsConfig.jsx
@@ -2,8 +2,8 @@
 import { CognitoUserPool } from 'amazon-cognito-identity-js';
 
 const poolData = {
-    UserPoolId: process.env.REACT_APP_USER_POOL_ID, // e.g., us-east-1_XXXXX
-    ClientId: process.env.REACT_APP_CLIENT_ID, // e.g., 2jqdp3hj3example
+    UserPoolId: process.env.REACT_APP_USER_POOL_ID,
+    ClientId: process.env.REACT_APP_CLIENT_ID,
 };
 
 export const userPool = new CognitoUserPool(poolData);
