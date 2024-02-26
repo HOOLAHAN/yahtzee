4a4d42ab7f9bee682606ce53cf5069aba10c71bd
Iain <iain.hoolahan@gmail.com>
2024-02-26 15:08:41 +0000

    formatting lines

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,9 @@
 import './tailwind.css';
 import Navbar from './components/Navbar';
 import Game from './components/Game';
-
+import config from './aws-exports';
 import { Amplify } from 'aws-amplify';
 import { withAuthenticator } from '@aws-amplify/ui-react';
-
-import config from './aws-exports';
 
 Amplify.configure(config);
 
