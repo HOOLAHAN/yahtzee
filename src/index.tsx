3237db5a5531c1558406b77a3ed5f6c3293a81b1
Iain <iain.hoolahan@gmail.com>
2024-02-26 13:25:53 +0000

    remove amplify import from index

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { Amplify } from 'aws-amplify';
-import amplifyConfig from './amplify-config';
 
-Amplify.configure(amplifyConfig as any);
-
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
   <React.StrictMode>
     <App />
