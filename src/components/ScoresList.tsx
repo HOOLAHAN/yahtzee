db56a5025ccccf7264bf063af2f54e685737fc26
Iain <iain.hoolahan@gmail.com>
2024-02-27 17:12:46 +0000

    created component to list scores

---
+++ src/components/ScoresList.tsx
@@ -0,0 +1,60 @@
+// ScoresList.tsx
+
+import React, { useEffect, useState } from 'react';
+import { Amplify } from 'aws-amplify';
+import { generateClient } from 'aws-amplify/api';
+import config from '../amplifyconfiguration.json';
+import { listScores } from '../graphql/queries';
+
+Amplify.configure(config);
+const client = generateClient();
+
+interface ScoreItem {
+  id: string;
+  username: string;
+  score: number;
+}
+
+const ScoresList: React.FC = () => {
+  const [scores, setScores] = useState<ScoreItem[]>([]);
+
+  useEffect(() => {
+    const fetchScores = async () => {
+      try {
+        const result = await client.graphql({
+          query: listScores
+        });
+        if ('data' in result && result.data && result.data.listScores && result.data.listScores.items) {
+          const scoresList: ScoreItem[] = result.data.listScores.items;
+          setScores(scoresList);
+        } else {
+          throw new Error('Failed to fetch scores.');
+        }
+      } catch (error) {
+        console.error('Error fetching scores:', error);
+      }
+    };
+    
+
+    fetchScores();
+  }, []);
+
+  return (
+    <div>
+      <h3>Scores List</h3>
+      {scores.length ? (
+        <ul>
+          {scores.map((score) => (
+            <li key={score.id}>
+              User: {score.username} - Score: {score.score}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No scores available.</p>
+      )}
+    </div>
+  );
+};
+
+export default ScoresList;
