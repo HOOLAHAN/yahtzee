e942a932c6d2c61becc1ebd790ab01e689dd92c4
Iain <iain.hoolahan@gmail.com>
2025-05-28 19:01:58 +0100

    improve button appearance

--- src/components/game/GameControlButtons.tsx
+++ src/components/game/GameControlButtons.tsx
@@ -1,5 +1,3 @@
-// GameControlButtons.tsx
-
 import CreateScoreButton from './CreateScoreButton';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFilePdf } from '@fortawesome/free-solid-svg-icons';
@@ -27,17 +25,23 @@ const GameControlButtons: React.FC<GameControlButtonsProps> = ({
   const { toggleRefreshLeaderboard } = useLeaderboardRefresh();
 
   return (
-    <div className={`flex ${isMobile ? 'flex-col space-y-2' : 'flex-row space-x-2'} mt-4`}>
+    <div className={`flex ${isMobile ? 'flex-col space-y-3' : 'flex-row space-x-4'} mt-6 justify-center`}>
+      {/* Reset Game Button */}
       <button 
-        className="transition duration-300 ease-in-out transform hover:scale-105 py-2 px-4 w-full md:w-auto bg-red-600 text-white rounded hover:bg-red-700 focus:ring focus:ring-red-200"
+        className="px-4 py-2 rounded-md font-bold text-red-400 border border-red-500 bg-deepBlack
+                   hover:text-white hover:bg-red-800 hover:border-red-400
+                   transition duration-300 transform hover:scale-105 shadow-[0_0_10px_#ff0000]"
         onClick={onResetGame}
       >
         {isMobile ? "Reset" : "Reset Game"}
       </button>
 
+      {/* Score Card Button */}
       {usedCategories === 13 && (
         <button 
-          className="w-full md:w-auto transition duration-300 ease-in-out transform hover:scale-105 py-2 px-4 bg-blue-600 text-white rounded hover:bg-blue-700 focus:ring focus:ring-blue-200"
+          className="px-4 py-2 rounded-md font-bold text-deepBlack bg-neonYellow
+                     hover:brightness-110 transition duration-300 transform hover:scale-105 
+                     shadow-[0_0_10px_#faff00]"
           onClick={() => printDocument(isTwoPlayer)}
         >
           <FontAwesomeIcon icon={faFilePdf} className="mr-2" />
@@ -45,6 +49,7 @@ const GameControlButtons: React.FC<GameControlButtonsProps> = ({
         </button>
       )}
 
+      {/* Submit Score to Leaderboard Button */}
       {usedCategories === 13 && isUserSignedIn && !isTwoPlayer && (
         <CreateScoreButton
           score={totalScore}
